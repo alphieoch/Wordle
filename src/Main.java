09d8cb1c9708497e3d006fee818848ded0abc930
Alphonce Ochieng <alphieoch@gmail.com>
2022-04-18 21:48:59 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//get the Dark mode to work on time.
+import java.time.LocalTime;
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Wordle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 250);
+        frame.setVisible(true);
+
+    }
+}
