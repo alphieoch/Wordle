e7c059e981f72c0ce384c259bb37bf3294875995
Alphonce Ochieng <alphieoch@gmail.com>
2022-04-19 22:06:59 +0100

    Now it randomly generate Text from a text file and will output it correctly.

--- src/RTG.java
+++ src/RTG.java
@@ -2,26 +2,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 class RTG{
-    public boolean Text(){
+    public String Text() {
         //Creating a linked list in an array
         List<String> TextList = new ArrayList<>();
-        try{
+        String RandomText = null;
+        try {
             File TextFile = new File("gameDictionary.txt");
             Scanner TextReader = new Scanner(TextFile);
             while (TextReader.hasNextLine()) {
                 String Textdata = TextReader.nextLine();
                 TextList.add(Textdata);
-        }
-            System.out.println(TextList);
+            }
+            //System.out.println(TextList);
+            //Noe getting the random element in the array
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(TextList.size());
+            RandomText = TextList.get(randomIndex);
+            //System.out.println(RandomText);
 
 
-    } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("File is missing please enter the file");
             e.printStackTrace();
         }
-        return false;}
+        return RandomText;
+    }
 }
 
