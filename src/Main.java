dcfe7c64657d866064c7331120074e06f3f7dd89
Alphonce Ochieng <alphieoch@gmail.com>
2022-04-19 22:08:34 +0100

    Now it randomly generate Text from a text file and will output it correctly.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
 ////Main part
 public class Main {
     public static void main(String args[]){
         RTG text = new RTG();
-        System.out.println(text.Text());
+        String Final_text = String.valueOf(text.Text());
+        //System.out.println(Final_text);
+        System.out.println(Final_text);
+
+        //System.out.println(text.Text());
     }
     }
